2be2f83536a65f2bbce8b8a63274731716f5e0ed
arruti <arruti@wizeline.com>
2016-09-22 20:52:03 -0500

    First home page version

---
+++ wp-content/themes/madera-makers/templates/content-home-page.php
@@ -0,0 +1,43 @@
+<?php the_content(); ?>
+<div class="jumbotron">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 text-center upside-down">
+        <h1 class="">Fabricamos el mueble que quieres</h1>
+        <!-- <p class="font-size-large">Learn User Experience Design from Wizeline’s world-class User Experience Team</p> -->
+        <ul class="list-inline row">
+          <li class="col-sm-4">
+            <div class="thumbnail">
+              <p class="">
+                <i class="fa fa-bed" aria-hidden="true"></i>
+                <strong>Paso 1</strong>
+              </p>
+            </div>
+            <h4>Sube la imagen del mueble de tus sueños</h4>
+          </li>
+          <li class="col-sm-4">
+            <div class="thumbnail">
+              <p>
+                <i class="fa fa-file-text-o" aria-hidden="true"></i>
+                <strong>Paso 2</strong>
+              </p>
+            </div>
+            <h4>Recibe tu cotización</h4>
+          </li>
+          <li class="col-sm-4">
+            <div class="thumbnail">
+              <p>
+                <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                <strong>Paso 3</strong>
+              </p>
+            </div>
+            <h4>Ordena tu mueble!</h4>
+          </li>
+        </ul>
+        <p>
+          <a href="https://facebook.com/maderamakers" class="btn btn-lg btn-info text-uppercase">Pide tu cotización!</a>
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
