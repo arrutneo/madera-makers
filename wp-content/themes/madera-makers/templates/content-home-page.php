347369580abd99bcb4217761ee560c1b9f441eda
arruti <arruti@wizeline.com>
2017-08-10 21:11:19 -0500

    Updated home page

--- wp-content/themes/madera-makers/templates/content-home-page.php
+++ wp-content/themes/madera-makers/templates/content-home-page.php
@@ -2,8 +2,8 @@
   <div class="container">
     <div class="row">
       <div class="col-xs-12 text-center upside-down">
-        <h1 class="text-uppercase">Fabricamos tu mueble a la medida</h1>
-        <p><em>En Madera Makers fabricamos, diseñamos y personalizamos cualquier tipo de mobiliario que tengas en mente.</em></p>
+        <h1 class="text-uppercase">Fabricamos tu mobiliario a medida</h1>
+        <p><em>En Madera Makers fabricamos y personalizamos cualquier tipo de proyecto en carpintería que tengas en mente.</em></p>
         <p class="vertical-separator">
           <a href="#formulario-pedido" class="btn btn-lg btn-primary">Pide tu cotización ahora</a>
         </p>
