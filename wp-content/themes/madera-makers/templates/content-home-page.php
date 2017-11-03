8bbf0e8e42429e83a219ff9097645137a4f2a730
arruti <arruti@wizeline.com>
2017-11-03 15:52:33 -0600

    correct form ID

--- wp-content/themes/madera-makers/templates/content-home-page.php
+++ wp-content/themes/madera-makers/templates/content-home-page.php
@@ -62,9 +62,9 @@
       <div class="panel panel-default box-shadow">
         <?php
         if(isset($_SERVER['WP_DB_PASSWORD'])){
-          echo do_shortcode('[contact-form-7 id="6" title="Envíanos tu idea"]');
-        } else {
           echo do_shortcode('[contact-form-7 id="7" title="Envíanos tu idea"]');
+        } else {
+          echo do_shortcode('[contact-form-7 id="11" title="Envíanos tu idea"]');
         }
         ?>
       </div>
